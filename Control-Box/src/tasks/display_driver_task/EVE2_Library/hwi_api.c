78ab92520a4c0c5c107f5edc0d13f1ad2989d0c7
byork6 <byork041@outlook.com>
2025-03-30 20:44:43 -0500

    Remoed comments & added memset()

--- Control-Box/src/tasks/display_driver_task/EVE2_Library/hwi_api.c
+++ Control-Box/src/tasks/display_driver_task/EVE2_Library/hwi_api.c
@@ -1,18 +1,16 @@
 #include "../../../common/common.h"
 
-// TODO: Write the HAL api functions based off the EVE examples
-SPI_Handle gSpiHandle;
-/* HAL_SPI_Enable() is to drive the CS Pin to the eve HIGH */
+// Globarl Vars
+SPI_Handle g_spiHandle;
+
 void HAL_SPI_Enable(void){
     GPIO_write(CONFIG_DISPLAY_CS, 0);
 }
 
-/* HAL_SPI_Disable() is to drive the CS Pin to the eve LOW */
 void HAL_SPI_Disable(void){
     GPIO_write(CONFIG_DISPLAY_CS, 1);
 }
 
-/* HAL_SPI_Write(uint8_t data) does a single byte SPI write transfer */
 uint8_t HAL_SPI_Write(uint8_t data){
     SPI_Transaction transaction;
     uint8_t rxBuf = 0;
@@ -21,47 +19,43 @@ uint8_t HAL_SPI_Write(uint8_t data){
     transaction.txBuf = &data;
     transaction.rxBuf = &rxBuf;
 
-    if (SPI_transfer(gSpiHandle, &transaction)) {
+    if (SPI_transfer(g_spiHandle, &transaction)) {
         return rxBuf;
     } else {
         return 0xFF;  // This return statement shows that there is a failure
     }
 }
 
-/* HAL_SPI_WriteBuffer does a buffer based SPI Write transfer */
 void HAL_SPI_WriteBuffer(uint8_t *Buffer, uint32_t Length){
     SPI_Transaction transaction;
+    memset(&transaction, 0, sizeof(transaction));
 
     transaction.count = Length;
     transaction.txBuf = Buffer;
     transaction.rxBuf = NULL;
 
-    SPI_transfer(gSpiHandle, &transaction);
+    SPI_transfer(g_spiHandle, &transaction);
 }
 
-/* HAL_SPI_WriteBuffer does a buffer based SPI Read transfer */
 void HAL_SPI_ReadBuffer(uint8_t *Buffer, uint32_t Length){
     SPI_Transaction transaction;
     uint8_t dummyTx = 0x00;
 
     transaction.count = Length;
-    transaction.txBuf = &dummyTx;  // Repeated dummy writes
+    transaction.txBuf = &dummyTx;
     transaction.rxBuf = Buffer;
 
-    SPI_transfer(gSpiHandle, &transaction);
+    SPI_transfer(g_spiHandle, &transaction);
 }
 
-/* Stall the cpu for X milliseconds */
 void HAL_Delay(uint32_t milliSeconds){
-    Task_sleep((milliSeconds * 1000) / Clock_tickPeriod);
+    Task_sleep(DEFAULT_TASK_SLEEP_DURATION);
 }
 
-/* Gives an opertunity to reset the EVE hardware */
 void HAL_Eve_Reset_HW(void){
-    
+    // Optional --- Toggles reset GPIO pin if one is being used.
 }
 
-/* Cleans up and resources allocated */
 void HAL_Close(void){
-    SPI_close(gSpiHandle);
+    SPI_close(g_spiHandle);
 }
