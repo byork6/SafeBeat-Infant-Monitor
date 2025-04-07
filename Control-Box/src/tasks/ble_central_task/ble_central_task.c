06305ede952bfd99830e8e1b3ac8a466689423eb
Brian York <byork041@outlook.com>
2025-04-07 01:07:22 -0500

    Keep working on undefined symbols

--- Control-Box/src/tasks/ble_central_task/ble_central_task.c
+++ Control-Box/src/tasks/ble_central_task/ble_central_task.c
@@ -1,7 +1,10 @@
 #include "common.h"
 
 // VARIABLE DECLARATIONS
-static uint8_t addrMode = DEFAULT_ADDRESS_MODE;
+// BLE user defined configuration
+icall_userCfg_t user0Cfg = BLE_USER_CFG;
+// Address Mode
+static uint8_t addrMode = DEFAULT_ADDRESS_MODE;// BLE user defined configuration
 
 // FUNCTION DEFINITIONS
 Task_Handle bleCentral_constructTask(){
@@ -22,7 +25,8 @@ void bleCentral_executeTask(UArg arg0, UArg arg1) {
     (void)arg0;
     (void)arg1;
     static int i = 0;
-    
+
+    printf("Initializing BLE Central...\n");
     bleCentral_init();
 
     printf("Entering bleCentral_executeTask()...\n");
@@ -37,21 +41,23 @@ void bleCentral_executeTask(UArg arg0, UArg arg1) {
 }
 
 void bleCentral_init(void){
+    // Initialize ICall module
+    printf("ICall_init\n");
+    ICall_init();
+    printf("ICall_createRemoteTasks\n");
+    ICall_createRemoteTasks(); // Creates stack and service task
+
     // Register with stack to receive GAP and GATT messages
+    printf("ICall_registerApp\n");
     ICall_registerApp(&selfEntity, NULL);
 
     // Initialize GATT
+    printf("GATT_InitClient\n");
     GATT_InitClient();
+    printf("GATT_RegisterForMsgs\n");
     GATT_RegisterForMsgs(ICALL_SERVICE_CLASS_BLE);
 
     // Set the GAP Role (this sets the device as Central)
+    printf("GAP_DeviceInit\n");
     GAP_DeviceInit(GAP_PROFILE_CENTRAL, selfEntity, addrMode, pRandomAddress);
 }
-
-// void  *MAP_l2capFindLocalCID( uint16 CID ){
-
-// }
-
-// void MAP_l2capGetCoChannelInfo(void *pCoC, void *pInfo ){
-
-// }
