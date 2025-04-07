8575e7dd00865bfd32b6d95a5396905a474ce1f4
Charan Kotrike <charan.kotrike74@gmail.com>
2025-04-07 16:40:40 -0500

    render display

--- Control-Box/src/tasks/display_driver_task/display_driver.c
+++ Control-Box/src/tasks/display_driver_task/display_driver.c
@@ -71,6 +71,8 @@ void displayDriver_executeTask(UArg arg0, UArg arg1) {
         GPIO_write(SD_SPI_CSN_PIN, 1);
         GPIO_write(DISPLAY_SPI_CSN_PIN, 0);
         
+        renderDisplay(testHeartRate, testRespirationRate); //added this line to call the function
+
         Task_sleep(g_taskSleepDuration);
     }
 }
@@ -86,6 +88,7 @@ void ClearScreen(void){
 	HAL_Delay(10);
 }
 
+//unsure if this is test code or code for when display starts up -Charan
 void MakeScreen_MatrixOrbital(uint8_t DotSize){
 	Send_CMD(CMD_DLSTART);                   //Start a new display list
 	Send_CMD(VERTEXFORMAT(0));               //setup VERTEX2F to take pixel coordinates
@@ -104,13 +107,45 @@ void MakeScreen_MatrixOrbital(uint8_t DotSize){
 	UpdateFIFO();                            // Trigger the CoProcessor to start processing the FIFO
 }
 
+//i worked on this
 void renderDisplay(int heartRate, int respirationRate) {
-    printf("HR: %d  |  RR: %d\n", heartRate, respirationRate);
+    printf("HR: %d  |  RR: %d\n", heartRate, respirationRate); //for debugging
 
-    if (heartRate < LOW_HEART_RATE_THRESHOLD_BPM || heartRate > HIGH_HEART_RATE_THRESHOLD_BPM ||
-        respirationRate < LOW_RESPIRATORY_RATE_THRESHOLD_BRPM || respirationRate > HIGH_RESPIRATORY_RATE_THRESHOLD_BRPM) {
-        printf("ALARM: Abnormal vitals detected!\n");
+    int screenWidth = Display_Width();
+    int screenHeight = Display_Height();
+
+    Send_CMD(CMD_DLSTART);
+    Send_CMD(CLEAR_COLOR_RGB(0, 0, 0)); // Black background
+    Send_CMD(CLEAR(1, 1, 1));
+
+    // Title bar
+    Send_CMD(COLOR_RGB(0, 255, 255)); // Cyan title
+    Cmd_Text(screenWidth / 2, screenHeight / 10, 30, OPT_CENTER, "INFANT ECG MONITOR");
+
+    // Heart Rate
+    char hrBuffer[50];
+    sprintf(hrBuffer, "Heart Rate: %d BPM", heartRate);
+    Send_CMD(COLOR_RGB(255, 255, 255)); // White
+    Cmd_Text(screenWidth / 2, screenHeight / 3, 31, OPT_CENTER, hrBuffer);
+
+    // Alarm Logic
+    bool alarm = (heartRate < LOW_HEART_RATE_THRESHOLD_BPM || heartRate > HIGH_HEART_RATE_THRESHOLD_BPM ||
+                  respirationRate < LOW_RESPIRATORY_RATE_THRESHOLD_BRPM || respirationRate > HIGH_RESPIRATORY_RATE_THRESHOLD_BRPM);
+
+    if (alarm) {
+        Send_CMD(COLOR_RGB(255, 0, 0)); // Red text
+        Cmd_Text(screenWidth / 2, (2 * screenHeight) / 3, 30, OPT_CENTER, "ALERT: VITALS OUT OF RANGE");
+
+        // Red blinking indicator (you can later toggle visibility every other render)
+        Send_CMD(POINT_SIZE(30 * 16)); // Circle size
+        Send_CMD(BEGIN(POINTS));
+        Send_CMD(VERTEX2F((screenWidth / 2) * 16, (8 * screenHeight / 10) * 16)); // Lower center
+        Send_CMD(END());
     }
+
+    Send_CMD(DISPLAY());
+    Send_CMD(CMD_SWAP);
+    UpdateFIFO();
 
     // We need to replace printf() with FT813 draw-text commands once graphics integration starts
 }
